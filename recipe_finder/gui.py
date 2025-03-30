780e6b718f8425822a31736439cd5c6017a364ef
Hardik S <hardikgowda0810@gmail.com>
2025-03-30 14:32:42 +0000

    Changed the file structure to be more simple

---
+++ recipe_finder/gui.py
@@ -0,0 +1,6 @@
+import tkinter as tk
+
+win = tk.Tk()
+win.title("Hello")
+win.geometry("200x200")
+win.mainloop()
--- recipe_finder/gui/main_window.py
+++
@@ -1,6 +0,0 @@
-import tkinter as tk
-
-win=tk.Tk()
-
-button=tk.Button("Click here")
-button.pack()
